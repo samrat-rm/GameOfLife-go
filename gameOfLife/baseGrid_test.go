b474f70e198cd763599a18143d3c2aa556b12a5a
Samrat <smaratrm2@gmail.com>
2023-05-09 02:43:16 +0530

    grid update method created and tested

--- gameOfLife/baseGrid_test.go
+++ gameOfLife/baseGrid_test.go
@@ -55,3 +55,24 @@ func TestCreateGrid(t *testing.T) {
 		}
 	}
 }
+func TestUpdateGrid(t *testing.T) {
+	baseGrid := NewBaseGrid(5, 5)
+	baseGrid.createGrid()
+	oldGrid := baseGrid.Grid
+
+	baseGrid.UpdateGrid()
+	newGrid := baseGrid.Grid
+
+	if oldGrid == nil {
+		t.Error("Old grid should not be nil")
+	}
+	if newGrid == nil {
+		t.Error("New grid should not be nil")
+	}
+	if len(oldGrid) != len(newGrid) {
+		t.Error("Old grid and new grid should have the same number of rows")
+	}
+	if len(oldGrid[0]) != len(newGrid[0]) {
+		t.Error("Old grid and new grid should have the same number of columns")
+	}
+}
